6f3f62542c4a6ca45c3db0fe13bd35cef96d2d4b
RolfHaeckel <rolf.haeckle@mac.com>
2024-11-01 12:50:47 +0100

    materialize css eingebunden

--- views/employee/table.php
+++ views/employee/table.php
@@ -1,6 +1,12 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/company.css" media="screen,projection"/>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -9,7 +15,7 @@
 </head>
 <body>
 <?php include 'views/navigation.php'; ?>
-<table>
+<table class="highlight">
     <tr>
         <th>Vorname</th>
         <th>Nachname</th>
@@ -20,19 +26,24 @@
     </tr>
 
     <?php
-    foreach ($employees as $m ) :
+    foreach ($employees as $m) :
         ?>
         <tr>
             <td><?php echo $m->getFirstName(); ?></td>
             <td><?php echo $m->getLastName(); ?></td>
             <td><?php echo $m->getGender(); ?></td>
-            <td><?php echo number_format($m->getSalary(),2, ',',''); ?></td>
-            <td><a href="index.php?action=delete&area=employee&id=<?php echo $m->getId(); ?>"><button>Löschen</button></a></td>
-            <td><a href="index.php?action=showEdit&area=employee&id=<?php echo $m->getId(); ?>"><button>Ändern</button></a></td>
+            <td><?php echo number_format($m->getSalary(), 2, ',', ''); ?></td>
+            <td><a href="index.php?action=delete&area=employee&id=<?php echo $m->getId(); ?>">
+                    <button>Löschen</button>
+                </a></td>
+            <td><a href="index.php?action=showEdit&area=employee&id=<?php echo $m->getId(); ?>">
+                    <button>Ändern</button>
+                </a></td>
         </tr>
-        <?php
+    <?php
     endforeach;
     ?>
 </table>
+<script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 </html>
