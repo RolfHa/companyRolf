449214f6d9444e087ab84ed547fa03117c210e29
RolfHaeckel <rolf.haeckle@mac.com>
2024-11-05 11:17:29 +0100

    pulldown bei views/rental.php eingebaut

--- views/rental/edit.php
+++ views/rental/edit.php
@@ -9,20 +9,7 @@ $r = (!$rentalDataExists) ? new Rental() : $r;
     <input type="hidden" name="area" value="<?php echo $area; ?>">
     <input type="hidden" name="id" value="<?php echo ($r instanceof Rental && $rentalDataExists === true) ? $r->getId() : ''; ?>">
     <table>
-<!--        <tr><td><label>Mitarbeiter </td><td>--><?php //echo $r->getEmployeePulldown(); ?><!--</label></td></tr>-->
-        <tr><td><label>Mitarbeiter </td><td>
-                <div class="input-field col s12">
-                    <select id="form-select-1">
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                    </select>
-                    <label for="form-select-1">Materialize Select</label>
-                </div>
-            </td></tr>
-
-
+        <tr><td><label>Mitarbeiter </td><td><?php echo $r->getEmployeePulldown(); ?></label></td></tr>
         <tr><td><label>Kennzeichen: </td><td><?php echo $r->getCarPulldown(); ?></label></td></tr>
         <tr><td><label>von: </td><td><input name="startDate" type="datetime-local"  value="<?php echo ($r instanceof Rental && $rentalDataExists === true) ? $r->getStartDate() : ''; ?>"></label></td></tr>
         <tr><td><label>bis: </td><td><input name="endDate" type="datetime-local"  value="<?php echo ($r instanceof Rental && $rentalDataExists === true) ? $r->getEndDate() : ''; ?>"></label></td></tr>
